a8b09ca4b0f7857008610c2847183ec901b94fd6
Sergio <sergioparilli06@gmail.com>
2025-04-27 00:06:35 -0400

    up

--- src/components/ShowHide.jsx
+++ src/components/ShowHide.jsx
@@ -2,14 +2,14 @@ import React, { useState } from "react"
 import "./ShowHide.css"
 
 function ShowHide() {
-    const [show, setShow] = React.useState(true)
+    const [show, setShow] = React.useState(false)
 
     return (
         <div className="show-hide">
             <button onClick={() => setShow(!show)}>
                 {show ? "Ocultar" : "Mostrar"} Foto
             </button>
-            {show && <img src="./public/foto.jpg" alt="foto" />}
+            {show && <img src="./foto.jpg" alt="foto" />}
             </div>
     );
 }
--- src/data/colchones.js
+++ src/data/colchones.js
@@ -2,37 +2,37 @@ const colchones = [
     {
         name: "Esmeralda",
         description: "Colchón de Resorte Semi-Ortopedico",
-        image: "./public/Esmeralda.jpg",
+        image: "./Esmeralda.jpg",
         price: "$200",
     },
     {
         name: "Diamante",
         description: "Colchón de Resorte Ortopedico",
-        image: "./public/Esmeralda.jpg",
+        image: "./Esmeralda.jpg",
         price: "$300",
     },
     {
         name: "Zafiro",
         description: "Colchón de Espuma",
-        image: "./public/Esmeralda.jpg",
+        image: "./Esmeralda.jpg",
         price: "$150",
     },
     {
         name: "Rubí",
         description: "Colchón de Espuma Viscoelástica",
-        image: "./public/Esmeralda.jpg",
+        image: "./Esmeralda.jpg",
         price: "$250",
     },
     {
         name: "Topacio",
         description: "Colchón de Espuma de Alta Densidad",
-        image: "./public/Esmeralda.jpg",
+        image: "./Esmeralda.jpg",
         price: "$180",
     },
     {
         name: "Perla",
         description: "Colchón de Espuma con Gel",
-        image: "./public/Esmeralda.jpg",
+        image: "./Esmeralda.jpg",
         price: "$220",
     },
 ]
